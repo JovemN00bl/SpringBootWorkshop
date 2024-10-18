245e27a51881d648cfd62f1565e04dd7c9244151
Admar Junior <admarjunior56@gmail.com>
2024-10-18 12:45:31 -0300

    OrderStatus enum

--- src/main/java/application/springbootworkshop/config/TestConfig.java
+++ src/main/java/application/springbootworkshop/config/TestConfig.java
@@ -3,6 +3,7 @@ package application.springbootworkshop.config;
 
 import application.springbootworkshop.entities.Order;
 import application.springbootworkshop.entities.User;
+import application.springbootworkshop.entities.enums.OrderStatus;
 import application.springbootworkshop.repositories.OrderRepository;
 import application.springbootworkshop.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, u1, Instant.parse("2019-06-20T19:53:07Z"));
-        Order o2 = new Order(null, u2, Instant.parse("2019-07-21T03:42:10Z"));
-        Order o3 = new Order(null, u1, Instant.parse("2019-07-22T15:21:22Z"));
+        Order o1 = new Order(null, u1, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID);
+        Order o2 = new Order(null, u2, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT);
+        Order o3 = new Order(null, u1, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/application/springbootworkshop/entities/Order.java
+++ src/main/java/application/springbootworkshop/entities/Order.java
@@ -1,5 +1,6 @@
 package application.springbootworkshop.entities;
 
+import application.springbootworkshop.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jdk.jfr.Enabled;
@@ -26,13 +27,17 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order() {}
 
-    public Order(Long id, User client, Instant moment) {
+    public Order(Long id, User client, Instant moment, OrderStatus orderStatus) {
         this.id = id;
         this.client = client;
         this.moment = moment;
+        setOrderStatus(orderStatus);
     }
+
 
     public Long getId() {
         return id;
@@ -56,6 +61,17 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus!=null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+
     }
 
     @Override
